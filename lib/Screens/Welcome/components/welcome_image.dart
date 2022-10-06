8702aaf02b2e9e75711c2836de94bbb46a41b5f5
osykris <osy.krisdayanti27@gmail.com>
2022-10-06 18:02:27 +0700

    ui welcome page

--- lib/Screens/Welcome/components/welcome_image.dart
+++ lib/Screens/Welcome/components/welcome_image.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../constants.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 class WelcomeImage extends StatelessWidget {
   const WelcomeImage({
@@ -12,49 +10,27 @@ class WelcomeImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-          child: SafeArea(
-              child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return Column(
+      children: [
+        Text(
+          "Welcome to sbmedia.id",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: defaultPadding * 2),
+        Row(
           children: [
-            Column(
-              children: [
-                Text(
-                  'Welcome to sbmedia.id!',
-                  style: title,
-                ),
-                
-              ],
-            ),   
+            Spacer(),
+            Expanded(
+              flex: 8,
+              child: Image.asset("assets/images/logo.png",  width: 300,             
+              height: 80,
+            ),
+            ),
+            Spacer(),
           ],
         ),
-      ))),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedIconTheme: IconThemeData(color: purple, size: 35),
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: const Color(0xffF4F6F9),
-        unselectedItemColor: const Color(0xFFB7BEF5),
-        // currentIndex: _currentIndex,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.folder_sharp),
-            label: 'Document',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera_alt),
-            label: 'Scan',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Setting',
-          ),
-        ],
-      ),
+        SizedBox(height: defaultPadding * 2),
+      ],
     );
   }
 }
--- lib/Screens/Welcome/welcome_screen.dart
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -1,139 +1,68 @@
 import 'package:flutter/material.dart';
 
-import '../../../constants.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../../components/background.dart';
+import '../../responsive.dart';
+import 'components/login_signup_btn.dart';
+import 'components/welcome_image.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
+    return Background(
+      child: SingleChildScrollView(
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(40),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          child: Responsive(
+            desktop: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text(
-                  'Welcome to sbmedia.id!',
-                  style: title,
+                const Expanded(
+                  child: WelcomeImage(),
                 ),
-                Padding(padding: EdgeInsets.only(top: 30)),
-                Center(
-                  child: Column(
-                    children: [
-                      Image.asset("assets/images/banner.png",
-                          width: 320, height: 150, fit: BoxFit.fill),
-                      Padding(padding: EdgeInsets.only(top: 20)),
-                      Text(
-                        'Indonesia"s No.1 Leading Sustainability Media Platform',
-                        style: desc,
-                      )
-                    ],
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(top: 25)),
-                Text(
-                  'Top News',
-                  style: TextStyle(
-                      color: darkBlue,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16),
-                ),
-                Padding(padding: EdgeInsets.only(top: 25)),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
+                Expanded(
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Image.asset("assets/images/new1.png",
-                          width: 100, height: 100, fit: BoxFit.fill),
-                      Padding(padding: EdgeInsets.only(right: 6)),
-                      Image.asset("assets/images/new2.png",
-                          width: 100, height: 100, fit: BoxFit.fill),
-                      Padding(padding: EdgeInsets.only(right: 6)),
-                      Image.asset("assets/images/new3.png",
-                          width: 100, height: 100, fit: BoxFit.fill),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      SizedBox(
+                        width: 450,
+                        child: LoginAndSignupBtn(),
+                      ),
                     ],
                   ),
-                ),
-                Padding(padding: EdgeInsets.only(top: 30)),
-                Text(
-                  'New Event',
-                  style: TextStyle(
-                      color: darkBlue,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16),
-                ),
-                Padding(padding: EdgeInsets.only(top: 25)),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Navigating ESG',
-                              style: TextStyle(
-                                  color: darkBlue,
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 12),
-                            ),
-                            Padding(padding: EdgeInsets.only(top: 15)),
-                            Text(
-                              'Zoom Meeting',
-                              style: desc,
-                            ),
-                            Text(
-                              'Selasa, 04 April 2022',
-                              style: desc,
-                            ),
-                          ],
-                        ),
-                        Padding(padding: EdgeInsets.only(right: 55)),
-                        Image.asset("assets/images/event.png",
-                            width: 160, height: 100, fit: BoxFit.fill),
-                      ],
-                    ),
-                  ],
                 ),
               ],
             ),
+            mobile: const MobileWelcomeScreen(),
           ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedIconTheme: IconThemeData(color: Color(0xFFFFFFFF), size: 35),
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: '#40A57C'.toColor(),
-        type: BottomNavigationBarType.fixed,
-        unselectedItemColor: const Color(0xFFB7BEF5),
-        // currentIndex: _currentIndex,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            label: "Home",
-            icon: Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(
-            label: "News",
-            icon: Icon(Icons.book),
-          ),
-          BottomNavigationBarItem(
-            label: "Events",
-            icon: Icon(FontAwesomeIcons.calendar),
-          ),
-          BottomNavigationBarItem(
-            label: "Login",
-            icon: Icon(Icons.login),
-          ),
-        ],
-      ),
+    );
+  }
+}
+
+class MobileWelcomeScreen extends StatelessWidget {
+  const MobileWelcomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const WelcomeImage(),
+        Row(
+          children: const [
+            Spacer(),
+            Expanded(
+              flex: 8,
+              child: LoginAndSignupBtn(),
+            ),
+            Spacer(),
+          ],
+        ),
+      ],
     );
   }
 }
